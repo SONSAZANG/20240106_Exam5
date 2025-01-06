1b38f5b499267e11a4f657d9d6bbeee74f82d447
soneunsu <kekekee257@gmail.com>
2025-01-06 19:18:46 +0900

    [필수 기능 구현]

---
+++ Source/Exam05/Exam05.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Exam05.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Exam05, "Exam05" );
---
+++ Source/Exam05/MyActor.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyActor.h"
+
+// Sets default values
+AMyActor::AMyActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AMyActor::Move()
+{
+	start.X += Step();
+	start.Y += Step();
+
+	UE_LOG(LogTemp, Display, TEXT("플레이어가 이동했습니다. 현재 좌표 X: %f, Y: %f"), start.X, start.Y);
+}
+
+int AMyActor::Step()
+{
+	int rand = FMath::RandRange(0, 1);
+	return rand;
+}
+
+// Called when the game starts or when spawned
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	start = FVector2D(0, 0);
+	for (int i = 0; i < 10; i++) {
+		Move();
+	}
+}
+
+// Called every frame
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
